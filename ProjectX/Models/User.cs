2eeda50ff02a4b4560fac2f215d68595f3362545
Jonathan Andersson <jonathan@ezy.se>
2012-09-17 10:45:13 +0200

    Added the database

--- ProjectX/Models/User.cs
+++ ProjectX/Models/User.cs
@@ -12,15 +12,17 @@ namespace ProjectX.Models
     {
 
         public int Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
         public string GravatarEmail { get; set; }
-        public string GitHubUserName { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }        
+        public string GitHub { get; set; }
         public bool DisplayEmail { get; set; }
-        public DateTime Joined { get; set; }
-        public string UserName { get; set; }
+        public DateTime Created { get; set; }
+        
         public string Description { get; set; }
 
         public string Image(ProfileImageSize imageSize)
