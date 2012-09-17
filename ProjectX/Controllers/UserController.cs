f88dfc13fadc0c913587487d79388efeab5c8af9
Jonathan Andersson <jonathan@ezy.se>
2012-09-17 10:16:05 +0200

    Renamed member to user

---
+++ ProjectX/Controllers/UserController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectX.Models;
+using ProjectX.Helpers;
+
+namespace ProjectX.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int id, string userName)
+        {
+            var user = new User().GetFakeUser();
+
+            var userViewModel = new UserViewModel
+            {
+                DisplayEmail = user.DisplayEmail,
+                DisplayName = user.DisplayName,
+                Email = user.Email,
+                FirstName = user.FirstName,
+                GitHubUserName = user.GitHubUserName,
+                GravatarEmail = user.GravatarEmail,
+                Joined = user.Joined,
+                LastName = user.LastName,
+                Id = user.Id,
+            };
+
+            string expectedName = HelperService.GenerateSlug(user.UserName);
+            string actualName = (userName ?? "").ToLower();
+
+            // permanently redirect to the correct URL
+            if (expectedName != actualName)
+            {
+                return RedirectToActionPermanent("details", "user", new { id = user.Id, userName = expectedName });
+            }
+
+            return View(userViewModel);
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectX/Models/User.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjectX.Helpers;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectX.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GravatarEmail { get; set; }
+        public string GitHubUserName { get; set; }
+        public bool DisplayEmail { get; set; }
+        public DateTime Joined { get; set; }
+        public string UserName { get; set; }
+        public string Description { get; set; }
+
+        public string Image(ProfileImageSize imageSize)
+        {
+            return HelperService.GetGravatar(GravatarEmail, imageSize);
+        }
+        private string _displayName;
+        public string DisplayName
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(_displayName) ? _displayName : UserName;
+            }
+            set
+            {
+                _displayName = value;
+            }
+        }
+
+        public User GetFakeUser()
+        {
+            return new User
+            {
+                DisplayEmail = true,
+                DisplayName = "Jonathan Andersson",
+                Email = "ja1984@gmail.com",
+                FirstName = "Jonathan",
+                LastName = "Andersson",
+                GravatarEmail = "ja1984@gmail.com",
+                UserName = "ja1984",
+                Id = 1
+            };
+        }
+    }
+
+    public enum ProfileImageSize
+    {
+        Small = 20,
+        Normal = 40,
+        Large = 80,
+        Extreme = 280
+    }
+
+    public class UserRegisterModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Gravatar email address")]
+        public string GravatarEmail { get; set; }
+
+        [Display(Name = "Github username")]
+        public string GitHubUserName { get; set; }
+        
+        [Display(Name="Display my email")]
+        public bool DisplayEmail { get; set; }
+    }
+
+    public class UserViewModel
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string DisplayName { get; set; }
+        public string GravatarEmail { get; set; }
+        public string GitHubUserName { get; set; }
+        public bool DisplayEmail { get; set; }
+        public DateTime Joined { get; set; }
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+
+}
