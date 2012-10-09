e544a965cd930722346b2bb0b5142c4f9f9d8f9d
Fredrik Olsson <fredrik@mrolsson.se>
2012-10-09 14:31:57 +0200

    Deleted PDFsharp+MigraDoc, installed iTextSharp

---
+++ ProjectX/Models/pdf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectX.Models
+{
+    public class pdf
+    {
+        public int id { get; set; }
+        public UserViewModel User { get; set; }
+        public string Message { get; set; }
+        public string Adress { get; set; }
+        public int Zipcode { get; set; }
+        public string City { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Cellphone { get; set; }
+    }
+}
