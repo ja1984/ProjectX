ac24ddf6827f8146272c6881a499087ea04c8514
Jonathan <ja1984@gmail.com>
2012-10-15 00:23:26 +0200

    Added some semicolons

--- ProjectX/Scripts/gitModel.js
+++ ProjectX/Scripts/gitModel.js
@@ -6,40 +6,39 @@
         var inner = {};
         inner.message = item.commit.message;
         return inner;
-    }
+    };
 
-    pub.viewModel = function (config) {
-        var inner = {}
+    pub.viewModel = function(config) {
+        var inner = { };
 
         inner.commits = ko.observableArray([]);
         inner.showAllCommits = ko.observable(false);
 
-        inner.getCommits = function () {
+        inner.getCommits = function() {
             $.ajax({
                 url: "https://api.github.com/repos/" + config.user + "/" + config.repository + "/commits",
                 data: "",
-                success: function (response) {
+                success: function(response) {
                     var i = 0;
-                    $.each(response, function () {
+                    $.each(response, function() {
                         inner.commits.push(new priv.Commit(this));
                     });
                 },
-                error: function () {
+                error: function() {
                     $(".github").hide();
                 },
                 dataType: "json"
             });
-        }
-
-        inner.toggleShowAllCommits = function () {
+        };
+        inner.toggleShowAllCommits = function() {
             inner.showAllCommits = !inner.showAllCommits();
-        }
+        };
 
 
         inner.getCommits();
 
         return inner;
-    }
+    };
 
     return pub;
 };
